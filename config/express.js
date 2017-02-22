60060f538780cdb81703e95a602765076c1cb55d
Supasate Choochaisri <supasate.c@gmail.com>
2017-02-23 01:02:34 +0700

    Run code based on environment

--- config/express.js
+++ config/express.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const bodyParser = require('body-parser')
 
 module.exports = function() {
   const app = express()
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+  } else {
+    app.use(compression)
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true,
+  }))
+  app.use(bodyParser.json())
+
   require('../app/routes/index.routes')(app)
 
   return app
