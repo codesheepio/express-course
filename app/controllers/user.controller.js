0c34791b731faa7b42d7f730a32d88a8e50d3a0f
Supasate Choochaisri <supasate.c@gmail.com>
2017-02-23 16:04:56 +0700

    Redirect to index after logging in

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,5 +1,6 @@
 exports.login = function(req, res) {
-  console.log(req.body)
-  console.log('Email: ' + req.body.email)
-  console.log('Password: ' + req.body.password)
+  res.render('index', {
+    title: 'Logged in as ' + req.body.email,
+    isLoggedIn: true,
+  })
 }
